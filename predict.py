f1d91db35397547c6c09bb007971176595f9fa61
Harish <149600195+Harish-Chandar@users.noreply.github.com>
2025-11-19 21:37:20 -0600

    Add TensorFlow model for digit prediction

--- predict.py
+++ predict.py
@@ -1,3 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers, optimizers
+import cv2
+import numpy as np 
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer=optimizers.Adam(0.001),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.load_weights("./model.weights.h5")
+
+
 def preprocess_for_mnist(frame):
     """
     Convert a Raspberry Pi camera frame → MNIST-like 28x28 image.
